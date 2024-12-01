b6ac9ed7769814d96037530381e562c341df0d74
aryadary0706 <112590882+aryadary0706@users.noreply.github.com>
2024-12-01 19:52:22 +0700

    Update Databuku.h

--- Databuku.h
+++ Databuku.h
@@ -1,57 +1,64 @@
 #ifndef DATABUKU_H_INCLUDED
 #define DATABUKU_H_INCLUDED
-#include <iostream>
 
+#include <iostream>
 using namespace std;
+
+//List child
 struct Buku{
-    int IDBuku;
-    string Judul;
+    int IDBuku; //ID yang dimiliki Buku yang terdaftar
+    string Judul; //Judul yang dimiliki Buku
     string Penerbit;
     int Tahunterbit;
     int Cetakan;
     string Editor;
 };
 
+//list Parent
 struct Penulis{
-    int IDPenulis;
+    int IDPenulis; // ID yang dimiliki Penulis yang ada dalam list
     string nama;
     string asal;
-    string namaPena;
+    string namaPena; //opsional. Jika penulis tidak ada nama pena. Maka ditulis '-'
 };
 
 typedef struct ElmBuku *AdrBuku;
 typedef struct ElmPenulis *AdrPenulis;
 typedef struct ElmRelasi *AdrRelasi;
 
+//ElmBuku bersifat double linked list
 struct ElmBuku {
-    Buku InfoBuku;
+    Buku InfoBuku; //Informasi Buku
     AdrBuku next;
     AdrBuku prev;
 };
 
+//ElmPenulis bersifat single linked list
 struct ElmPenulis {
-    Penulis InfoPen;
+    Penulis InfoPen; //Informasi Penulis
     AdrPenulis next;
 };
 
 struct ElmRelasi {
-    AdrBuku BR;
-    AdrPenulis PR;
+    AdrBuku BR; //BookRelation: Address untuk AdrBuku, menyimpan alamat Buku dalam ListBuku
+    AdrPenulis PR; //PenulisRelation: Address untuk adrPenulis, menyimpan alamat Buku dalam ListPenulis
     AdrRelasi next;
 };
 
+//List bersifat double linked list
 struct ListBuku {
     AdrBuku First;
     AdrBuku Last;
 };
-
+//List bersifat single linked list
 struct ListPenulis {
     AdrPenulis First;
 };
-
+//List bersifat single linked list
 struct ListRelasi {
     AdrRelasi First;
 };
+
 
 void createListPenulis(ListPenulis &P);
 void createListBuku(ListBuku &B);
@@ -64,14 +71,12 @@ AdrBuku FindBukuByJudul(ListBuku B, string judul);
 AdrPenulis FindPenulisByName(ListPenulis P, string Nama);
 void InsertRelasi(ListRelasi &R, AdrRelasi NewRelasi);
 void AddRelasi(ListRelasi &R, string JudulBuku, ListBuku B, ListPenulis P, string NamaPenulis);
-void HapusPenulis(ListPenulis &P, ListRelasi &R, int IDPenulis);
-void HapusBuku(ListBuku &B, ListRelasi &R, int IDBuku);
-void TampilkanSemuaBuku(ListBuku B, ListRelasi R);
-void TampilkanBukuByPenulis(ListRelasi R, string NamaPenulis);
-void TampilkanPenulisByBuku(ListRelasi R, string JudulBuku);
-void TampilkanPenulisPalingAktifDanPasif(ListPenulis P, ListRelasi R);
-void TampilkanPenulisAscending(ListPenulis &P);
-void TampilkanPenulisDescending(ListPenulis &P);
-
+void DeleteBuku(ListBuku &B, string BukuDihapus);
+void DeletePenulis(ListPenulis &G, string PenulisDihapus);
+void PrintBukuDanPenulis(ListRelasi R);
+void PrintBukuBasedPenulis(ListRelasi R, string NamaPenulis);
+void PrintPenulisOnBuku(ListRelasi R, string judulBuku);
+void EvaluasiPenulis();
+void sortingBuku();
 
 #endif // DATABUKU_H_INCLUDED
