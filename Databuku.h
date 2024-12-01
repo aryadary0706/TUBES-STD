7c7c613f476ada5af0468153d498411b4e368b9e
aryadary0706 <112590882+aryadary0706@users.noreply.github.com>
2024-12-01 18:06:22 +0700

    Update Databuku.h

--- Databuku.h
+++ Databuku.h
@@ -3,52 +3,58 @@
 
 #include <iostream>
 using namespace std;
+
+//List child
 struct Buku{
-    int IDBuku;
-    string Judul;
+    int IDBuku; //ID yang dimiliki Buku yang terdaftar
+    string Judul; //Judul yang dimiliki Buku
     string Penerbit;
     int Tahunterbit;
     int Cetakan;
     string Editor;
 };
 
+//list Parent
 struct Penulis{
-    int IDPenulis;
+    int IDPenulis; // ID yang dimiliki Penulis yang ada dalam list
     string nama;
     string asal;
-    string namaPena;
+    string namaPena; //opsional. Jika penulis tidak ada nama pena. Maka ditulis '-'
 };
 
 typedef struct ElmBuku *AdrBuku;
 typedef struct ElmPenulis *AdrPenulis;
 typedef struct ElmRelasi *AdrRelasi;
 
+//ElmBuku bersifat double linked list
 struct ElmBuku {
-    Buku InfoBuku;
+    Buku InfoBuku; //Informasi Buku
     AdrBuku next;
     AdrBuku prev;
 };
 
+//ElmPenulis bersifat single linked list
 struct ElmPenulis {
-    Penulis InfoPen;
+    Penulis InfoPen; //Informasi Penulis
     AdrPenulis next;
 };
 
 struct ElmRelasi {
-    AdrBuku BR;
-    AdrPenulis PR;
+    AdrBuku BR; //BookRelation: Address untuk AdrBuku, menyimpan alamat Buku dalam ListBuku
+    AdrPenulis PR; //PenulisRelation: Address untuk adrPenulis, menyimpan alamat Buku dalam ListPenulis
     AdrRelasi next;
 };
 
+//List bersifat double linked list
 struct ListBuku {
     AdrBuku First;
     AdrBuku Last;
 };
-
+//List bersifat single linked list
 struct ListPenulis {
     AdrPenulis First;
 };
-
+//List bersifat single linked list
 struct ListRelasi {
     AdrRelasi First;
 };
