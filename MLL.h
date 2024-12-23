c967370327f1d7e44cfc78d91ea9832cbfbfe974
aryadary0706 <112590882+aryadary0706@users.noreply.github.com>
2024-12-23 08:51:55 +0700

    Update MLL.h

--- MLL.h
+++ MLL.h
@@ -3,7 +3,7 @@
 
 #include "MLL_child.h"
 #include "MLL_parent.h"
-#include "MLL_relation.h"
+#include "relation.h"
 
 void EvaluasiPenulis(ListPenulis P, ListRelasi R);
 void AddRelasi(ListRelasi &R, string JudulBuku, ListBuku B, ListPenulis P, string NamaPenulis);
@@ -11,5 +11,7 @@ void HapusPenulisPadaRelasi(ListPenulis &P, ListRelasi &R, int IDPenulis);
 void HapusBukuPadaRelasi(ListBuku &B, ListRelasi &R, int IDBuku);
 void InputBukukeList(ListBuku &B, AdrBuku book, ListRelasi &R);
 void InputPenuliskeList(ListPenulis &P, AdrPenulis Pen, ListRelasi &R);
+void TambahRelasiPenulis(ListRelasi &R, ListBuku B, ListPenulis P);
+void TambahRelasiBuku(ListRelasi &R,ListBuku B,ListPenulis P);
 
 #endif // MLL_H_INCLUDED
